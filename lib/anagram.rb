22e3746cefe64d95a853950a0691012c49fbe6ce
Vicki Hamm <vicki.cello@gmail.com>
2018-06-26 14:40:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,21 @@
 class Anagram
 
+  attr_accessor :word
 
   def initialize(word)
     @word = word
   end
 
-  
+  def match(array)
+    array.select do |w|
+      split_word = w.split("").sort
+      original_word = @word.split("").sort
+      if split_word == original_word
+        w
+      end
+      end
+    end
+
+
+
+end
